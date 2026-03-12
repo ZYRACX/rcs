85ed95722fec17e77acad8a341278f5a919430b1
ZYARCX <soumya.lodh206@gmail.com>
2026-03-12 18:41:37 +0530

    feat(admin): Implement item management CRUD controller functions

--- server/src/modules/admin/itemsManager/itemManager.controller.js
+++ server/src/modules/admin/itemsManager/itemManager.controller.js
@@ -21,3 +21,97 @@ export async function getAllItems(req, res){
         })
     }catch(error){}
 }
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ */
+export async function addItem(req, res){
+    try{
+
+        const {itemName, itemAltId, itemBaseValue, chanceOfGetting, wayToObtain} = req.body
+
+        const {tablesDB} = createAppwriteClient("admin")
+
+        const result = await tablesDB.createRow({
+            databaseId: appwrite.appwrite.databaseId,
+            tableId: appwrite.appwrite.ITEM_TABLE,
+            rowId: ID.unique(),
+            data: {
+                itemName,
+                itemAltId,
+                itemBaseValue,
+                chanceOfGetting,
+                wayToObtain
+            }
+        })
+        if(!result.$id) return res.status("500").message
+
+        return res.status(200).json({
+            message: "Item has successfully added to the database.",
+            newItemId: result.$id
+        })
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ */
+export async function updateItem(req, res) {
+    try{
+        const {itemId} = req.params
+        const {itemName, itemAltId, itemBaseValue, chanceOfGetting, wayToObtain} = req.body
+        // console.log(itemName, wayToObtain)
+
+        const {tablesDB} = createAppwriteClient("admin")
+        const result = await tablesDB.updateRow({
+            databaseId: appwrite.appwrite.databaseId,
+            tableId: appwrite.appwrite.ITEM_TABLE,
+            rowId: itemId,
+            data: {
+                itemName,
+                itemAltId,
+                itemBaseValue,
+                chanceOfGetting,
+                wayToObtain
+            }
+        }).catch((error) => {
+            console.log("Error when trying to update a item", error)
+        })
+
+        
+    }catch(error){}
+}
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ */
+export async function deleteItem(req, res) {
+    try{
+    const {itemId} = req.params
+
+    const {tablesDB} = createAppwriteClient("admin")
+
+    const result = await tablesDB.deleteRow({
+        databaseId: appwrite.appwrite.databaseId,
+        tableId: appwrite.appwrite.ITEM_TABLE,
+        rowId: itemId
+    })
+
+    return res.status(200).json({
+        message: "The item has successfully deleted from the database."
+    })
+
+    }catch(error) {
+        console.log("Error during deletion of an item: " + error)
+    }
+    
+
+}
